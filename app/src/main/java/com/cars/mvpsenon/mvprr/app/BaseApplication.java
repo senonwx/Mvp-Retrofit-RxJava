07f8fc12921113b47cbb966c341b82776bd80462
senonwx <1083911695@qq.com>
2017-11-10 09:18:24 +0800

    first commit

--- app/src/main/java/com/cars/mvpsenon/mvprr/app/BaseApplication.java
+++ app/src/main/java/com/cars/mvpsenon/mvprr/app/BaseApplication.java
@@ -5,7 +5,19 @@ import android.app.Application;
 import android.content.Context;
 import android.os.StrictMode;
 
+import com.cars.mvpsenon.R;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 import com.tencent.bugly.beta.Beta;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -33,6 +45,26 @@ public class BaseApplication extends Application {
         // 调试时，将第三个参数改为true
         com.tencent.bugly.Bugly.init(this, "f0ab571284", true);
     }
+
+    //static 代码段可以防止内存泄露
+//    static {
+//        //设置全局的Header构建器
+//        SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
+//            @Override
+//            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+//                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+//                return new ClassicsHeader(context).setTimeFormat(new SimpleDateFormat("更新于 mm-dd hh:mm"));//指定为经典Header，默认是 贝塞尔雷达Header
+//            }
+//        });
+//        //设置全局的Footer构建器
+//        SmartRefreshLayout.setDefaultRefreshFooterCreater(new DefaultRefreshFooterCreater() {
+//            @Override
+//            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+//                //指定为经典Footer，默认是 BallPulseFooter
+//                return new ClassicsFooter(context).setDrawableSize(20);
+//            }
+//        });
+//    }
 
     @Override
     protected void attachBaseContext(Context base) {
---
+++ app/src/main/java/com/cars/mvpsenon/refreshlayout/RefreshActivity1.java
@@ -0,0 +1,82 @@
+package com.cars.mvpsenon.refreshlayout;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cars.mvpsenon.R;
+import com.scwang.smartrefresh.header.MaterialHeader;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.footer.BallPulseFooter;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+public class RefreshActivity1 extends AppCompatActivity {
+    /*
+       * 关键代码，需要在布局生成之前设置，建议代码放在 Application 中
+       */
+    static {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
+            @NonNull
+            @Override
+            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+                ClassicsHeader header = new ClassicsHeader(context).setSpinnerStyle(SpinnerStyle.FixedBehind);
+                header.setPrimaryColorId(R.color.colorPrimary);
+                header.setAccentColorId(android.R.color.white);
+                return header;//指定为经典Header，默认是 贝塞尔雷达Header
+            }
+        });
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreater(new DefaultRefreshFooterCreater() {
+            @NonNull
+            @Override
+            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+                layout.setEnableLoadmoreWhenContentNotFull(true);//内容不满一页时候启用加载更多
+                ClassicsFooter footer = new ClassicsFooter(context);
+                footer.setBackgroundResource(android.R.color.white);
+                footer.setSpinnerStyle(SpinnerStyle.Scale);//设置为拉伸模式
+                return footer;//指定为经典Footer，默认是 BallPulseFooter
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refresh1);
+        final RefreshLayout refreshLayout = (RefreshLayout)findViewById(R.id.refreshLayout);
+        //设置 Header 为 Material风格
+        refreshLayout.setRefreshHeader(new MaterialHeader(this).setShowBezierWave(true));
+        //设置 Footer 为 球脉冲
+        refreshLayout.setRefreshFooter(new BallPulseFooter(this).setSpinnerStyle(SpinnerStyle.Scale));
+
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(2000);
+            }
+        });
+        refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadmore(2000);
+            }
+        });
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshLayout.autoLoadmore();
+            }
+        });
+    }
+}
